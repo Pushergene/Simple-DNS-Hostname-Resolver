e80c679af026e664e7ff56147ed47bebb4d857b9
Pushergene <pushergene@protonmail.com>
2017-11-20 09:55:50 +0100

    Add files via upload

---
+++ Test.rb
@@ -0,0 +1,9 @@
+﻿require 'resolv'
+require 'json'
+$address
+puts "Address: #{$address}"
+$address = gets.chomp
+Resolv::DNS.open do |dns|
+ress = dns.getresources "#{$address}", Resolv::DNS::Resource::IN::A
+  p ress.map { |r| r.address }
+end
